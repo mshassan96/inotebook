9048edca74e88281437a52ce544d694f06c374f0
mshassan96 <miansalmanhassan@yahoo.com>
2022-07-13 22:50:08 +0500

    Added Schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  passowrd: { type: String, required: true },
+  password: { type: String, required: true },
   date: { type: Date, default: Date.now },
 });
 
