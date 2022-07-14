21f777647ecbe8f6a7a136392da5779f2e1b79d4
mshassan96 <miansalmanhassan@yahoo.com>
2022-07-14 16:03:55 +0500

    Added models and validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,38 @@
 const express = require('express');
-const User = require('../models/User');
 const router = express.Router();
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-// Create a user using: POST "api/auth". Doesn't require Auth
+// Create a user using: POST "api/auth/createuser". No Login Required
 
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.post('/createuser', [
+    body('name', 'Enter a valid Name with more than 3 characters.').isLength({ min: 3 }),
+    body('email', 'Enter a valid email. Email must include @ sign.').isEmail(),
+    body('password', 'Password must be atlease 8 characters.').isLength({ min: 8 }),
+], async (req, res) => {
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+    try {
+        // Check if the user with same email exists already
+        let user = await User.findOne({email: req.body.email});
+        if (user) {
+            return res.status(400).json({error: "Duplicate email id."})
+        }
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+    
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
 })
 
 module.exports = router
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const Notes = require('../models/Notes');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
-    obj = {
-        id: 1,
-        name: "John"
-    };
-
-    res.json(obj);
+router.post('/', (req, res) => {
+    const notes = Notes(req.body);
+    notes.save();
+    res.send(req.body);
 })
 
 module.exports = router
