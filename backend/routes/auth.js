8793df95dbbe9d8eaedcd899cef2916de1148214
mshassan96 <miansalmanhassan@yahoo.com>
2022-07-15 01:08:31 +0500

    Added bcrypt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = '$alman';
 
 // Create a user using: POST "api/auth/createuser". No Login Required
-
 router.post('/createuser', [
     body('name', 'Enter a valid Name with more than 3 characters.').isLength({ min: 3 }),
     body('email', 'Enter a valid email. Email must include @ sign.').isEmail(),
@@ -22,13 +25,26 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({error: "Duplicate email id."})
         }
+
+        const salt = await bcrypt.genSalt(10);
+        secPass = await bcrypt.hash(req.body.password, salt);
+
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: secPass,
         });
-    
-        res.json(user);
+
+        const data = {
+            user: {
+                id: user.id,
+
+            }
+        }
+        
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({authToken});
+
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Some error occured");
